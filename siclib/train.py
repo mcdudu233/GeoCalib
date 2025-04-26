6f4d6a7489c24b4b00d01648ae63d19b4bdb7c89
dudu233 <1147793932@qq.com>
2025-04-26 14:10:46 +0800

    找到最佳学习率

--- siclib/train.py
+++ siclib/train.py
@@ -113,6 +113,8 @@ def get_lr_scheduler(optimizer, conf):
                 schedulers.append(scheduler)
 
             options = {k: v for k, v in conf.options.items() if k != "schedulers"}
+            if "lr_lambda" in options:
+                options["lr_lambda"] =lambda epoch: epoch // 2
             return getattr(torch.optim.lr_scheduler, conf.type)(optimizer, schedulers, **options)
 
         return getattr(torch.optim.lr_scheduler, conf.type)(optimizer, **conf.options)
