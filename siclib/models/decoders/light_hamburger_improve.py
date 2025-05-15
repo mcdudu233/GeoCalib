dd94da6616b74248f13917c0cb8191a71627b2fb
dudu233 <1147793932@qq.com>
2025-05-15 18:42:23 +0800

    新增geocalib-improve

--- siclib/models/decoders/light_hamburger_improve.py
+++ siclib/models/decoders/light_hamburger_improve.py
@@ -233,8 +233,8 @@ class LightHamHead(BaseModel):
         for idx, (hires_feat, freqfusion) in enumerate(zip(inputs[1:], self.freqfusions)):
             _, hires_feat, lowres_feat = freqfusion(hr_feat=hires_feat, lr_feat=lowres_feat)
             b, _, h, w = hires_feat.shape
-            lowres_feat = (torch.cat([hires_feat.reshape(b * self.feature_resample_group, -1, h, w),
-                                     lowres_feat.reshape(b * self.feature_resample_group, -1, h, w)],
+            lowres_feat = (torch.cat([hires_feat.reshape(b * 4, -1, h, w),
+                                     lowres_feat.reshape(b * 4, -1, h, w)],
                                     dim=1)
                            .reshape(b, -1, h, w))
 
