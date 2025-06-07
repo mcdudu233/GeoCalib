4db29858d1cbca8ce2948425c5fb3fb640c9a366
dudu233 <1147793932@qq.com>
2025-06-07 10:02:43 +0800

    测试查看结果

--- siclib/models/utils/modules.py
+++ siclib/models/utils/modules.py
@@ -158,16 +158,23 @@ class FeatureFusionBlock(nn.Module):
         output = xs[0]
 
         if len(xs) == 2:
-            tmp = self.resConfUnit1(xs[1])
-            fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(12, 24))
-            axes_flat = axes.flatten()
-            for i, ax in enumerate(axes_flat):
-                ax.imshow(tmp[0][i].detach().cpu(), cmap="gray")
-                ax.axis('off')
-            plt.show()
+            # tmp = self.resConfUnit1(xs[1])
+            # fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(12, 24))
+            # axes_flat = axes.flatten()
+            # for i, ax in enumerate(axes_flat):
+            #     ax.imshow(tmp[0][i].detach().cpu(), cmap="gray")
+            #     ax.axis('off')
+            # plt.show()
             output = output + self.resConfUnit1(xs[1])
 
         output = self.resConfUnit2(output)
+
+        fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(12, 24))
+        axes_flat = axes.flatten()
+        for i, ax in enumerate(axes_flat):
+            ax.imshow(output[0][i].detach().cpu(), cmap="gray")
+            ax.axis('off')
+        plt.show()
 
         if self.upsample:
             output = F.interpolate(output, scale_factor=2, mode="bilinear", align_corners=False)
